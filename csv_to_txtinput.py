7dd5eb84fe3c02e04bf02c9cb5458053b5e9922c
rafapmiranda <38067955+rafapmiranda@users.noreply.github.com>
2018-08-29 14:21:04 +0100

    Pre-processing script.

---
+++ csv_to_txtinput.py
@@ -0,0 +1,205 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 23 10:20:12 2018
+
+@author: Rafael
+"""
+#%%
+from __future__ import print_function
+import numpy as np
+np.random.seed(1337)
+
+import re
+import codecs
+from collections import defaultdict
+from nltk import sent_tokenize
+from collections import Counter
+
+#FUNCTIONS
+def clean(l_cid):
+    n_o_s = list(l_cid)
+    while True:
+        l_flatten = [line[0] for line in n_o_s if line != []]
+        l_freq = dict((k,v) for (k,v) in Counter(l_flatten).items())
+        o_s = set([k for (k,v) in l_freq.items() if v == 1])
+        print(len(o_s))
+        if len(o_s) == 0: break
+        for i in range(len(n_o_s)):
+            if n_o_s[i] != [] and n_o_s[i][0] in o_s:
+                if len(n_o_s[i]) > 1: n_o_s[i] = n_o_s[i][1:]
+                else: n_o_s[i] = ['']
+    return n_o_s
+    
+def codes(st):
+    s = st[:].replace('%','')
+    s = s.split()
+    icd = []
+    for word in s:
+        w = ""
+        for i in range(len(word)):
+            if word[i] in "0123456789" and word[-1] == '-' or ((word[i] == 'V' or word[i] == 'E') and len(word) >= 3 and word[1] in "0123456789"):
+                w = w + word[i]
+        icd.append(w)
+        for c in icd:
+            if c == "":
+                icd.remove("")
+    return icd
+
+def clean_num(sent):
+    aux = sent.split(' ')
+    ints = []
+    for word in aux:
+        numDigits = 0
+        for char in word:
+            if char.isdigit(): numDigits += 1
+        if len(word) > 0: digitFraction = numDigits / float(len(word))
+        else: digitFraction = 0
+        if word.isdigit(): ints.append(word)
+        elif digitFraction > 0.0: ints.append(word)
+    for num in ints: sent = sent.replace(num,'<NUMBER>', 1)
+    return sent
+
+def phrases(text):
+    p_lst = []
+    if text == "" or text == " ":
+        return p_lst
+    else:
+        for i in range(len(text)-1):
+            if ((text[i-1].isalpha() or text[i-1] == ")") and (text[i] == "." or text[i] == ";") and text[i+1] == ' '):
+                text = text[:i] + 'r2fa7m45ir' + text[i+1:]
+        p_lst = text.split("r2fa7m45ir ")
+    return p_lst
+
+#%%
+#HBA DISCHARGE NOTES
+print('Loading data...')
+
+texts = [line.split('\t') for line in [line for line in codecs.open('dataset_hba.txt', encoding="iso-8859-1")]]
+fields = texts.pop(0)
+texts = [line for line in texts if len(line) == 121 and not (line[15] == '' or line[15] == ' ')][:3000]
+
+print('Mapping ICD-9 codes...')
+labels_cid_pre = [codes(line[14]) for line in texts]
+labels_cid = clean(labels_cid_pre)
+
+#%%
+print('Preparing fields...')
+
+idade = [line[2] for line in texts]
+mot_int = [line[4].replace('<>','') for line in texts]
+res_clinic = [sent_tokenize(clean_num((line[8] + ' ' + line[9]).replace('%%%%%',' ').replace('<>',''))) for line in texts]
+#proc_diag = [sent_tokenize(line[13]) for line in texts]
+diag_alta = [[clean_num(re.sub(r'[^\w\s]','',diag)) for diag in line if diag != ''] for line in [field[15].replace('%%%%%','\n').replace('<>','').split('\n') for field in texts]]
+dep = [line[71] for line in texts]
+
+for i in range(len(dep)):
+    if dep[i] == 'Medicina':
+        dep[i] = (texts[i][71]+' '+texts[i][72]).replace('  ',' ').replace('  ',' ').replace('  ',' ').replace('  ',' ')
+        if dep[i] == 'Medicina ': dep[i] = 'Medicina Não especificado'
+    elif dep[i] == 'Cirurgia':
+        dep[i] = (texts[i][71]+' '+texts[i][73]).replace('  ',' ').replace('  ',' ').replace('  ',' ').replace('  ',' ')
+        if dep[i] == 'Cirurgia ': dep[i] = 'Cirurgia Não especificado'
+    elif dep[i] == ' ' or dep[i] == '':
+        dep[i] = 'Não especificado'
+
+#%%
+        
+for i in range(len(res_clinic)):
+    if len(res_clinic[i]) > 25: res_clinic[i] = res_clinic[i][:25]
+    if len(diag_alta[i]) > 9: diag_alta[i] = diag_alta[i][:9]
+    mot_int[i] = '873heGKe7I ' + mot_int[i] + ' 873heGKe7I'
+    for j in range(len(res_clinic[i])):
+        res_clinic[i][j] = '873heGKe7I ' + res_clinic[i][j] + ' 873heGKe7I'
+        if res_clinic[i] == '873heGKe7I  873heGKe7I' or res_clinic[i] == '873heGKe7I   873heGKe7I': res_clinic[i] = ''
+    for j in range(len(diag_alta[i])):
+        diag_alta[i][j] = '873heGKe7I ' + diag_alta[i][j] + ' 873heGKe7I'
+        if diag_alta[i] == '873heGKe7I  873heGKe7I' or diag_alta[i] == '873heGKe7I   873heGKe7I': diag_alta[i] = ''
+    while len(res_clinic[i]) < 25:
+        res_clinic[i].append('')
+    while len(diag_alta[i]) < 9:
+        diag_alta[i].append('')
+
+#%%
+print('Preparing discharge summaries file format...')
+
+texts_aux = list(res_clinic)
+for i in range(len(res_clinic)):
+    if labels_cid[i] != [''] and len(labels_cid[i]) != 0:
+        texts_aux[i] = labels_cid[i][0] + ' <> ' + diag_alta[i][0] + ' <> ' + diag_alta[i][1] + ' <> ' + diag_alta[i][2] + ' <> ' + diag_alta[i][3] + ' <> ' + diag_alta[i][4] + ' <> ' + diag_alta[i][5] + ' <> ' + diag_alta[i][6] + ' <> ' + diag_alta[i][7] + ' <> ' + diag_alta[i][8] + ' <> ' + mot_int[i] + ' <> ' + res_clinic[i][0] + ' <> ' + res_clinic[i][1] + ' <> ' + res_clinic[i][2] + ' <> ' + res_clinic[i][3] + ' <> ' + res_clinic[i][4] + ' <> ' + res_clinic[i][5] + ' <> ' + res_clinic[i][6] + ' <> ' + res_clinic[i][7] + ' <> ' + res_clinic[i][8] + ' <> ' + res_clinic[i][9] + ' <> ' + res_clinic[i][10] + ' <> ' + res_clinic[i][11] + ' <> ' + res_clinic[i][12] + ' <> ' + res_clinic[i][13] + ' <> ' + res_clinic[i][14] + ' <> ' +  res_clinic[i][15] + ' <> ' +  res_clinic[i][16] + ' <> ' + res_clinic[i][17] + ' <> ' + res_clinic[i][18] + ' <> ' + res_clinic[i][19] + ' <> ' + res_clinic[i][20] + ' <> ' + res_clinic[i][21] + ' <> ' + res_clinic[i][22] + ' <> ' + res_clinic[i][23] + ' <> ' + res_clinic[i][24] + ' <> ' + idade[i] + ' <> ' + dep[i] + ' <> ' + str(labels_cid[i])
+        #texts_aux[i] = labels_cid[i][0] + ' <> ' + diag_alta[i][0] + ' <> ' + diag_alta[i][1] + ' <> ' + diag_alta[i][2] + ' <> ' + diag_alta[i][3] + ' <> ' + diag_alta[i][4] + ' <> ' + diag_alta[i][5] + ' <> ' + diag_alta[i][6] + ' <> ' + diag_alta[i][7] + ' <> ' + diag_alta[i][8] + ' <> ' + mot_int[i] + ' <> ' + res_clinic[i][0] + ' <> ' + res_clinic[i][1] + ' <> ' + res_clinic[i][2] + ' <> ' + res_clinic[i][3] + ' <> ' + res_clinic[i][4] + ' <> ' + res_clinic[i][5] + ' <> ' + res_clinic[i][6] + ' <> ' + res_clinic[i][7] + ' <> ' + res_clinic[i][8] + ' <> ' + res_clinic[i][9] + ' <> ' + res_clinic[i][10] + ' <> ' + res_clinic[i][11] + ' <> ' + res_clinic[i][12] + ' <> ' + res_clinic[i][13] + ' <> ' + res_clinic[i][14] + ' <> ' +  res_clinic[i][15] + ' <> ' +  res_clinic[i][16] + ' <> ' + res_clinic[i][17] + ' <> ' + res_clinic[i][18] + ' <> ' + res_clinic[i][19] + ' <> ' + res_clinic[i][20] + ' <> ' + res_clinic[i][21] + ' <> ' + res_clinic[i][22] + ' <> ' + res_clinic[i][23] + ' <> ' + res_clinic[i][24] + ' <> ' + str(labels_cid[i])
+        #texts_aux[i] = labels_cid[i][0] + ' <> ' + res_clinic[i][0] + ' <> ' + res_clinic[i][1] + ' <> ' + res_clinic[i][2] + ' <> ' + res_clinic[i][3] + ' <> ' + res_clinic[i][4] + ' <> ' + res_clinic[i][5] + ' <> ' + res_clinic[i][6] + ' <> ' + res_clinic[i][7] + ' <> ' + res_clinic[i][8] + ' <> ' + dep[i] + ' <> ' + idade[i] + ' <> ' + str(labels_cid[i])
+        texts_aux[i] = texts_aux[i] + '\r'
+
+#%%
+print('Writing output...')
+
+f = open('dataset_example_hba_full.txt','w')
+
+for _list in texts_aux:
+    if isinstance(_list,str): f.write(_list)
+    
+f.close()
+
+#%%
+#MIMIC-III DISCHARGE NOTES
+print('Loading MIMIC-III data...')
+
+texts = [line.split(',',10) for line in codecs.open('dataset_mimic.txt', encoding="iso-8859-1")]
+texts = [[line[2],line[10]] for line in texts if ('History of Present Illness:'.lower() in line[10].lower() or 'History of the Present Illness:'.lower() in line[10].lower()) and 'Past Medical History:'.lower() in line[10].lower() and 'Allergies:'.lower() in line[10].lower() and 'Service:'.lower() in line[10].lower()]
+fields = ['Admission Date:','Discharge Date:','Date of Birth:','Sex:','Service:','Allergies:','Attending:','Chief Complaint:','Major Surgical or Invasive Procedure:','History of Present Illness:','History of the Present Illness:','Past Medical History:','Social History:','Family History:','Physical Exam:','Pertinent Results:','Brief Hospital Course:','Medications on Admission:','Discharge Medications:','Discharge Disposition:','Facility:','Discharge Diagnosis:','Discharge Condition:','Discharge Instructions:','Followup Instructions:']
+fields = ['Service:','Allergies:','History of Present Illness:','History of the Present Illness:','Past Medical History:']
+for i in range(len(texts)):
+    for field in fields:
+        texts[i][1] = texts[i][1].replace(field,'1y3g4jhk2g'+field).replace(field.upper(),'1y3g4jhk2g'+field)
+    texts[i][1] = texts[i][1].split('1y3g4jhk2g')
+texts = [line for line in texts if len(line[1]) == 5 and 'Service:'.lower() in line[1][1].lower() and ('History of Present Illness:'.lower() in line[1][3].lower() or 'History of the Present Illness:'.lower() in line[1][3].lower())]
+
+print('Mapping ICD-9 codes...')
+
+codes = [line.split(',') for line in codecs.open('dataset_mimic_codes.txt', encoding="utf-8")]
+codes = [[line[2], line[4].replace('\r\n','').replace('"','')] for line in codes]
+diagnoses_icd = defaultdict(list)
+for hadm_id, code in codes:
+    diagnoses_icd[hadm_id].append(code)
+
+for i in range(len(texts)):
+    texts[i][0] = diagnoses_icd[texts[i][0]] 
+
+labels_cid_no = [line[0] for line in texts]
+labels_cid = clean([line[0] for line in texts])
+
+print('Preparing department provenance...')
+
+texts = [line[1] for line in texts]
+dep = ['873heGKe7I ' + line[1].replace('Service:  ','').replace('Service: ','').replace('Service:','').lower() + ' 873heGKe7I' for line in texts]
+
+print('Preparing clinic resume...')
+
+res_clinic = [phrases(line[3].replace('History of Present Illness:','').replace('History of the Present Illness:','')) for line in texts]
+
+for i in range(len(res_clinic)):
+    if len(res_clinic[i]) > 20: res_clinic[i] = res_clinic[i][:20]
+    for j in range(len(res_clinic[i])):
+        res_clinic[i][j] = '873heGKe7I ' + res_clinic[i][j].lower() + ' 873heGKe7I'
+        if res_clinic[i] == '873heGKe7I  873heGKe7I' or res_clinic[i] == '873heGKe7I   873heGKe7I': res_clinic[i] = ''
+    while len(res_clinic[i]) < 20:
+        res_clinic[i].append('')
+    #res_clinic[i].append(dep[i])
+
+#%%
+print('Preparing discharge summaries file format...')
+
+texts_aux = res_clinic[:]
+for i in range(len(res_clinic)):
+    if len(labels_cid[i]) != 0 and labels_cid[i] != ['']:
+        texts_aux[i] = labels_cid[i][0] + ' <> ' + res_clinic[i][0] + ' <> ' + res_clinic[i][1] + ' <> ' + res_clinic[i][2] + ' <> ' + res_clinic[i][3] + ' <> ' + res_clinic[i][4] + ' <> ' + res_clinic[i][5] + ' <> ' + res_clinic[i][6] + ' <> ' + res_clinic[i][7] + ' <> ' + res_clinic[i][8] + ' <> ' + res_clinic[i][9] + ' <> ' + res_clinic[i][10] + ' <> ' + res_clinic[i][11] + ' <> ' + res_clinic[i][12] + ' <> ' + res_clinic[i][13] + ' <> ' + res_clinic[i][14] + ' <> ' +  res_clinic[i][15] + ' <> ' +  res_clinic[i][16] + ' <> ' + res_clinic[i][17] + ' <> ' + res_clinic[i][18] + ' <> ' + res_clinic[i][19] + ' <> ' + str(labels_cid[i])
+        texts_aux[i] = texts_aux[i].replace('.','')
+        texts_aux[i] = texts_aux[i] + '\r'
+
+print('Writing output...')
+f = open('dataset_input_mimic.txt','w')
+
+for _list in texts_aux:
+    if isinstance(_list,str): f.write(_list)
+    
+f.close()
--- dataset_analysis.py
+++ dataset_analysis.py
@@ -10,17 +10,21 @@ import numpy as np
 np.random.seed(1337)
 
 import codecs
+import re
 import csv
 import operator
+import matplotlib
 import matplotlib.pyplot as plt
+from nltk import sent_tokenize
 from collections import Counter
 from collections import OrderedDict
+from heatmap import heatmap, annotate_heatmap
 
 print('Loading data...')
 
 texts = [line.split('\t') for line in [line for line in codecs.open('dataset_hba.txt', encoding="iso-8859-1")]]
 fields = texts.pop(0)
-texts = [line for line in texts if len(line) == 120 and not (line[7] == '' or line[7] == ' ')]
+texts = [line for line in texts if len(line) == 121 and not (line[15] == '' or line[15] == ' ')]
 
 def clean(l_cid):
     n_o_s = list(l_cid)
@@ -37,13 +41,13 @@ def clean(l_cid):
     return n_o_s
 
 def codes(st):
-    s = st[:]
+    s = st[:].replace('%','')
     s = s.split()
     icd = []
     for word in s:
         w = ""
         for i in range(len(word)):
-            if word[i] in "0123456789" and word[-1] == '-' or (word[i] == 'V' and len(word) >= 3 and word[1] in "0123456789"):
+            if word[i] in "0123456789" and word[-1] == '-' or ((word[i] == 'V' or word[i] == 'E') and len(word) >= 3 and word[1] in "0123456789"):
                 w = w + word[i]
         icd.append(w)
         for c in icd:
@@ -90,30 +94,38 @@ def icd9_chap(lst):
             c_labels_cid[i] = 17
         elif c_labels_cid[i] >= 'V01' and c_labels_cid[i] <= 'V91': 
             c_labels_cid[i] = 18
-        elif c_labels_cid[i] >= 'E000' and c_labels_cid[i] <= 'E999': 
+        else: 
             c_labels_cid[i] = 19
     return c_labels_cid
+
+def clean_num(sent):
+    aux = sent.split(' ')
+    ints = []
+    for word in aux:
+        numDigits = 0
+        for char in word:
+            if char.isdigit(): numDigits += 1
+        if len(word) > 0: digitFraction = numDigits / float(len(word))
+        else: digitFraction = 0
+        if word.isdigit(): ints.append(word)
+        elif digitFraction > 0.0: ints.append(word)
+    for num in ints: sent = sent.replace(num,'<NUMBER>', 1)
+    return sent
 
 def count_phrases(text):
     p_lst = []
     if text == "" or text == " ":
         return p_lst
-    elif "." not in text:
-        return [text]
     else:
         for i in range(len(text)-1):
             if ((text[i-1].isalpha() or text[i-1] == ")") and (text[i] == "." or text[i] == ";") and text[i+1] == ' '):
-                text = text[:i] + 'r2p7m45' + text[i+1:]
-        text = text.lower()
-        for char in text:
-            if char not in "0123456789" and not char.isalpha() and char != " ":
-                text = text.replace(char,"")
-        p_lst = text.split("r2p7m45 ")
+                text = text[:i] + 'r2fa7m45ir' + text[i+1:]
+        p_lst = text.split("r2fa7m45ir ")
     return p_lst
 
 print('Mapping ICD-9 to ICD-10 codes...')
 
-labels_cid = clean([codes(line[13]) for line in texts])
+labels_cid = clean([codes(line[14]) for line in texts])
 labels_flatten = [item for sublist in labels_cid for item in sublist]
 blocks = sorted(list(set([item[:3] for item in labels_flatten])))
 chap = icd9_chap([x[:3] for x in labels_flatten])
@@ -125,39 +137,81 @@ print('  - Distinct ICD-9 chapters: %s' % len(set(chap)))
 
 #%%
 print('- Department provenance:')
-dep = [line[70] for line in texts]
+dep = [line[71] for line in texts]
+
 for i in range(len(dep)):
     if dep[i] == 'Medicina':
-        dep[i] = (texts[i][70]+' '+texts[i][71]).replace('  ',' ').replace('  ',' ').replace('  ',' ').replace('  ',' ')
+        dep[i] = (texts[i][71]+' '+texts[i][72]).replace('  ',' ').replace('  ',' ').replace('  ',' ').replace('  ',' ')
         if dep[i] == 'Medicina ': dep[i] = 'Medicina Não especificado'
     elif dep[i] == 'Cirurgia':
-        dep[i] = (texts[i][70]+' '+texts[i][72]).replace('  ',' ').replace('  ',' ').replace('  ',' ').replace('  ',' ')
+        dep[i] = (texts[i][71]+' '+texts[i][73]).replace('  ',' ').replace('  ',' ').replace('  ',' ').replace('  ',' ')
         if dep[i] == 'Cirurgia ': dep[i] = 'Cirurgia Não especificado'
     elif dep[i] == ' ' or dep[i] == '':
         dep[i] = 'Não especificado'
+
 dep_options = sorted(list(set(dep)))
+chap_set = list(set([x for x in chap if x != 19]))
+prov_chap = np.zeros((len(dep_options),len(chap_set)))
 
-prov = np.zeros((len(dep_options),len(blocks)))
-for aux in range(len(dep)):
-    i = dep_options.index(dep[aux])
-    for code in labels_cid[aux]:
-        j = blocks.index(code[:3])
-        prov[i][j] += 1
-
-prov_chap = np.zeros((len(dep_options),len(set(chap))+2))
 for aux in range(len(dep)):
     i = dep_options.index(dep[aux])
     for j in icd9_chap([x[:3] for x in labels_cid[aux]]):
-        if isinstance(j,int): prov_chap[i][j] += 1
+        if j < 19: prov_chap[i][j-1] += 1
 
-print(dep_options)
+fig, ax = plt.subplots(figsize=(20, 20))
+
+im, cbar = heatmap(prov_chap, dep_options, chap_set, ax=ax,
+                   cmap="Reds", cbarlabel="Prevalence")
+texts = annotate_heatmap(im, valfmt="{x:.0f}")
+
+plt.ylabel('Department', fontsize='large')
+ax.set_xlabel('ICD-9 chapters', fontsize='large')    
+ax.xaxis.set_label_position('top') 
+
+fig.tight_layout()
+fig.savefig('heatmap_dep.png',dpi=400)
+
+#%%
+print('- Age Groups:')
+idade = [int(line[2]) for line in texts]
+
+for i in range(len(idade)):
+    if idade[i] < 5: idade[i] = 0
+    elif idade[i] < 15: idade[i] = 1
+    elif idade[i] < 25: idade[i] = 2
+    elif idade[i] < 45: idade[i] = 3
+    elif idade[i] < 65: idade[i] = 4
+    else: idade[i] = 5
+    
+age_options = sorted(list(set(idade)))
+ag = ['0 - 4', '5 - 14', '15 - 24', '25 - 44', '45 - 64', '> 65']
+chap_set = list(set([x for x in chap if x != 19]))
+age_groups = np.zeros((len(age_options),len(chap_set)))
+
+for aux in range(len(idade)):
+    i = age_options.index(idade[aux])
+    for j in icd9_chap([x[:3] for x in labels_cid[aux]]):
+        if j < 19: age_groups[i][j-1] += 1
+
+fig, ax = plt.subplots(figsize=(15, 4))
+
+im, cbar = heatmap(age_groups, ag, chap_set, ax=ax,
+                   cmap="Reds", cbarlabel="Prevalence")
+texts = annotate_heatmap(im, valfmt="{x:.0f}")
+
+plt.ylabel('Age groups', fontsize='large')
+ax.set_xlabel('ICD-9 chapters', fontsize='large')    
+ax.xaxis.set_label_position('top') 
+
+fig.tight_layout()
+fig.savefig('heatmap_age.png',dpi=400)
 
 #%%
 print('\n- ICD-10 full-code statistics:')
 
 labels_freq = Counter(labels_flatten)
 labels_freq = sorted(labels_freq.items(), key=operator.itemgetter(1))[::-1]
-labels_freq = [tup for tup in labels_freq if tup[1] > 693]
+labels_freq = [tup for tup in labels_freq if tup[1] > 600 and tup[0] != '']
 labels_freq = OrderedDict(labels_freq)
 
 labels_fst = [line[0] for line in labels_cid if line != []]
@@ -187,14 +241,14 @@ fig.set_size_inches(20.0, 10.5)
 fig.savefig('labels_freq_50.png',dpi=400)
 
 #%%
-per_note = [len(lst) for lst in labels_cid]
-per_note_blk = [len(lst) for lst in [list(set([code[:3] for code in line])) for line in labels_cid]]
-per_note_chap = [len(lst) for lst in [list(set(icd9_chap(line))) for line in labels_cid]]
+per_note = [len(lst) for lst in labels_cid if lst != [] and lst != ['']]
+per_note_blk = [len(lst) for lst in [list(set([code[:3] for code in line])) for line in labels_cid if line != [] and line != ['']]]
+per_note_chap = [len(lst) for lst in [list(set(icd9_chap(line))) for line in labels_cid if line != [] and line != ['']]]
 
 print('\n  - ICD-9 codes per instance: %.3f' % np.mean(per_note))
 print('  - ICD-9 blocks per instance: %.3f' % np.mean(per_note_blk))
 print('  - ICD-9 chapters per instance: %.3f' % np.mean(per_note_chap))
-print('\n  - Number of instances: %s' % len(texts))
+print('\n  - Number of instances: %s' % len([x for x in labels_cid if x != [] and x != ['']]))
 print('  - Number of instances with multiple ICD-9 codes: %s' % len([l for l in per_note if l > 1]))
 
 per_note = OrderedDict(sorted(Counter(per_note).items()))
@@ -221,10 +275,10 @@ p3 = ax.bar(x3, k, width=0.2, color='salmon', align='center')
 ax.set_ylabel('Number of instances', fontsize='x-large')
 ax.set_xlabel('Number of occurences', fontsize='x-large')
 ax.set_title('Number of ICD-9 full-codes, blocks and chapters per note in the dataset')
-ax.set_xticks(list(range(max(per_note.keys())+1)))
+ax.set_xticks(list(range(1,max(per_note.keys())+1,1)))
 x1,x2,y1,y2 = ax.axis()
 ax.axis((-0.5,28.5,0,100000))
-ax.legend((p1[0], p2[0], p3[0]), ('ICD-10 Codes','ICD-10 Blocks','ICD-10 Chapters'), fontsize='xx-large')
+ax.legend((p1[0], p2[0], p3[0]), ('ICD-9 Codes','ICD-9 Blocks','ICD-9 Chapters'), fontsize='xx-large')
 
 fig.set_size_inches(20.0, 10.5)
 fig.savefig('code_per_note.png',dpi=400)
@@ -233,16 +287,16 @@ fig.savefig('code_per_note.png',dpi=400)
 print('\n- ICD-9 chapter statistics:')
 
 chap_freq = Counter((chap))
-chap_freq = dict((k,v) for (k,v) in chap_freq.items() if k != '')
+chap_freq = dict((k,v) for (k,v) in chap_freq.items() if k != 19)
 chap_freq = OrderedDict(sorted(chap_freq.items()))
 
-plt.bar(range(len(chap_freq)), chap_freq.values(), align='center')
+plt.bar(range(len(chap_freq)), chap_freq.values(), color='firebrick', align='center')
 plt.xticks(range(len(chap_freq)), list(chap_freq.keys()))
 plt.title('ICD-9 chapter frequency in the dataset')
 plt.xlabel('ICD-9 chapters')
 plt.ylabel('Number of occurences in the dataset')
 x1,x2,y1,y2 = plt.axis()
-plt.axis((-1,22,0,40000))
+plt.axis((-1,18,0,40000))
 
 fig = plt.gcf()
 fig.set_size_inches(20.0, 10.5)
@@ -250,20 +304,65 @@ fig.savefig('chap_freq.png',dpi=400)
 
 #%%
 print('- Textual statistics:')
+fields[120] = 'REQUISICAO_MEIO_TRANSPORTE'
 
-fields[119] = 'REQUISICAO_MEIO_TRANSPORTE'
+mot_int = [line[4].replace('<>','') for line in texts]
+res_clinic = [sent_tokenize(clean_num((line[8] + ' ' + line[9]).replace('%%%%%',' ').replace('<>',''))) for line in texts]
+diag_alta = [[clean_num(re.sub(r'[^\w\s]','',diag)) for diag in line if diag != ''] for line in [field[15].replace('%%%%%','\n').replace('<>','').split('\n') for field in texts]]
+
+input_text = [item for sublist in [diag_alta, mot_int, res_clinic] for item in sublist]
+#input_text = res_clinic
+
+stat_vec_words = [sum([len(sent.split(' ')) for sent in line]) for line in input_text]
+stat_vec_phrases = [len(x) for x in input_text]
+w_per_s = [0] * len(texts)
+
+for i in range(len(texts)):
+    if stat_vec_phrases[i] == 0: w_per_s[i] = 0
+    else: w_per_s[i] = stat_vec_words[i]/stat_vec_phrases[i]
+
+print('\n  - Sentences per instance(Average): %.3f' % np.mean(np.array(stat_vec_phrases)))
+print('  - Sentences per instance (90 percentile): %.3f' % np.percentile(np.array(stat_vec_phrases), 90))
+print('  - Words per instance (Average): %.3f' % np.mean(np.array(stat_vec_words)))
+print('  - Words per sentence (Average): %.3f' % np.mean(np.array(w_per_s)))
+print('  - Words per sentence (90 percentile): %.3f' % np.percentile(np.array(w_per_s), 90))
+
+#%%
+print('- Unfilled fields percentage:')
+
+unfilled = [0] * len(fields)
+
+for line in texts:
+    for i in range(len(line)):
+        if line[i] == '' or line[i] == ' ': unfilled[i] += 1
+        
+unfilled = [item*100/len(texts) for item in unfilled]
+
+plt.bar(range(len(unfilled)), unfilled, color='firebrick', align='center')
+plt.xticks(range(len(fields)), fields, rotation = "vertical")
+plt.title('Percentage of unfilled instances by discharge summary field')
+plt.xlabel('Discharge summary field')
+plt.ylabel('Unfilled percentage')
+x1,x2,y1,y2 = plt.axis()
+plt.axis((-1,121,0,100))
+
+fig = plt.gcf()
+fig.set_size_inches(30.0, 26.0)
+fig.savefig('unfilled.png',dpi=300)
+
+#%%
 stat_vec_words = [0] * len(fields)
 stat_vec_phrases = [0] * len(fields)
 unfilled = [0] * len(fields)
 for line in texts:
     for i in range(len(line)):
         if line[i] == '' or line[i] == ' ': unfilled[i] += 1
-        stat_vec_phrases[i] += len(count_phrases(line[i]))
+        stat_vec_phrases[i] += len(sent_tokenize(line[i]))
         stat_vec_words[i] += len(line[i].split(' '))
 
-print('\n  - Sentences per instance: %.3f' % (sum(stat_vec_phrases)/len(texts)))
-print('  - Words per instance: %.3f' % (sum(stat_vec_words)/len(texts)))
-print('  - Words per sentence: %.3f' % (sum(stat_vec_words)/sum(stat_vec_phrases)))
+print('\n  - Sentences per instance: %.3f' % (sum(stat_vec_phrases[7:9])/(len(texts))))
+print('  - Words per instance: %.3f' % (sum(stat_vec_words[7:9])/len(texts)))
+print('  - Words per sentence: %.3f' % (sum(stat_vec_words[7:9])/sum(stat_vec_phrases[7:9])))
 
 stat_vec_words = [int(round(x / len(texts))) for x in stat_vec_words]
 stat_vec_phrases = [int(round((x / len(texts)))) for x in stat_vec_phrases]
@@ -293,19 +392,3 @@ fig = plt.gcf()
 fig.set_size_inches(30.0, 26.0)
 fig.savefig('phrase_count.png',dpi=300)
 
-#%%
-print('- Unfilled fields percentage:')
-
-unfilled = [item*100/len(texts) for item in unfilled]
-
-plt.bar(range(len(unfilled)), unfilled, align='center')
-plt.xticks(range(len(fields)), fields, rotation = "vertical")
-plt.title('Percentage of unfilled instances by discharge summary field')
-plt.xlabel('Discharge summary field')
-plt.ylabel('Unfilled percentage')
-x1,x2,y1,y2 = plt.axis()
-plt.axis((-1,121,0,100))
-
-fig = plt.gcf()
-fig.set_size_inches(30.0, 26.0)
-fig.savefig('unfilled.png',dpi=300)
